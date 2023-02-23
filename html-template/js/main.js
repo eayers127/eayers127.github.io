4406273325e531c84f6b2a1b657c37895eb5b4e8
Ethan Ayers <ethan.ayers88@gmail.com>
2023-02-22 19:27:28 -0700

    Added slider, images, and icons

--- html-template/js/main.js
+++ html-template/js/main.js
@@ -1,16 +1,16 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    speed: 2000,
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    autoplay:{
-        delay:5000,
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  speed: 2000,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay:{
+      delay: 5000,
+  },
 
 });
 
